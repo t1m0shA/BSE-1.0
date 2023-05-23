8230aac30c18cf1e80bba249ab7d4ceb0c457e3b
Timofey Ermoshkin <timmiermoskin@gmail.com>
2023-05-23 07:22:20 +0300

    Функция создания матрицы

--- BSE 1.0/BSE 1.0.cpp
+++ BSE 1.0/BSE 1.0.cpp
@@ -27,3 +27,14 @@ void initializeArray(float** array, int size) {
         array[i] = new float[size];
     }
 }
+
+void createMatrix(float** array, int size) {
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            array[i][j] = -12 + rand() % (12 + 12 - 1);
+        }
+    }
+}
